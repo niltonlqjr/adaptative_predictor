6f1eafb3fc17d6b31e9400def7f43ba418980577
Nilton <niltonlqjr@gmail.com>
2022-10-04 14:39:23 -0300

    example script

--- run_example.sh
+++ run_example.sh
@@ -1,8 +1,5 @@
-1.create_sequences.sh
-2.collect_features.sh
-3.cluster.sh
-4.train_model.sh
-5.predict.sh
-run_example_old.sh
-run_example.sh
-vars.sh
+./1.create_sequences.sh
+./2.collect_features.sh
+./3.cluster.sh
+./4.train_model.sh
+./5.predict.sh
